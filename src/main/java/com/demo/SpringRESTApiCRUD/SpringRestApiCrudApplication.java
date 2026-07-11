0574d27bae7dd6fc61420b081cdb6c986f7cfb8c
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-07-11 12:44:37 -0400

    first commit

---
+++ src/main/java/com/demo/SpringRESTApiCRUD/SpringRestApiCrudApplication.java
@@ -0,0 +1,13 @@
+package com.demo.SpringRESTApiCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/SpringRESTApiCRUD/SpringRestApiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.SpringRESTApiCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
