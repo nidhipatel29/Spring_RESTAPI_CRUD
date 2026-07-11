bc36323e79032b6fdc47dfd68a4c91d412668c9b
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-07-11 15:25:02 -0400

    Move data initialization to @PostConstruct

--- src/main/java/com/demo/SpringRESTApiCRUD/rest/StudentRestController.java
+++ src/main/java/com/demo/SpringRESTApiCRUD/rest/StudentRestController.java
@@ -5,24 +5,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.SpringRESTApiCRUD.Entity.Student;
 
+import jakarta.annotation.PostConstruct;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    
-    @GetMapping("/student")
-    public List<Student> getStudent(){
-        List<Student> list=new ArrayList<>();
+
+    private List<Student> list;
+
+   @PostConstruct
+    public void loadData(){
+        list=new ArrayList<>();
         list.add(new Student("nidhi","patel"));
         list.add(new Student("paranshu","OG"));
         list.add(new Student("hemant","father"));
         list.add(new Student("rashmi","mother"));
+    }
+    
+    @GetMapping("/student")
+    public List<Student> getStudent(){
+       
         return list;
     } 
     
