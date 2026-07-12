f040dae8acb0c5d616e97b50a9c495001194c94f
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-07-12 13:50:39 -0400

    Add global exception handling using @ControllerAdvice

--- src/main/java/com/demo/SpringRESTApiCRUD/rest/StudentRestController.java
+++ src/main/java/com/demo/SpringRESTApiCRUD/rest/StudentRestController.java
@@ -1,18 +1,19 @@
 package com.demo.SpringRESTApiCRUD.rest;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.demo.SpringRESTApiCRUD.CustomException.StudentNotFoundException;
 import com.demo.SpringRESTApiCRUD.Entity.Student;
-
 import jakarta.annotation.PostConstruct;
-
 import java.util.ArrayList;
 import java.util.List;
-
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import com.demo.SpringRESTApiCRUD.ErrorResponce.StudentErrorResponce;
 
 
 
@@ -39,8 +40,15 @@ public class StudentRestController {
     
     @GetMapping("student/{studentId}")
     public Student getStudentById(@PathVariable int studentId){
+
+        //check again list size
+        if((studentId>list.size())|| studentId <0){
+           throw new StudentNotFoundException("Student not found" + studentId); 
+        }
        
         return list.get(studentId);
     }
+
+  
 
 }
